47de1c9dc522e92adc89516a6f71e23fd53f46ba
h_k_rony <raindropplus11@gmail.com>
2022-05-29 21:36:40 +0600

    project init

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='corse-goal'>
+      <h2>course goals</h2>
+      <ul className='goal-list'>
+        <li>finish the course</li>
+        <li>Learn all about the course main topic</li>
+        <li>Help other students in the course</li>
+      </ul>
+
+    </div>
+  );
+}
+
+export default App;
